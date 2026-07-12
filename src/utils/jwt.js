a5d51ddf6e9e6251128ec3400dbaf96bd154047f
soroush <soroushfalaki24@hotmail.com>
2026-07-12 11:37:37 +0330

    add jwt

---
+++ src/utils/jwt.js
@@ -0,0 +1,43 @@
+import jwt from 'jsonwebtoken';
+import { config } from '../config/index.js';
+
+export const createPayload = (user) => {
+  return { userId: user._id, role: user.role };
+};
+
+export const generateAccessToken = ({ payload }) => {
+  return jwt.sign(payload, config.JWT_ACCESS_TOKEN, {
+    expiresIn: config.ACCESS_TOKEN_EXPIRY,
+    subject: 'accessToken',
+  });
+};
+
+export const generateRefreshToken = ({ payload }) => {
+  return jwt.sign(payload, config.JWT_REFRESH_TOKEN, {
+    expiresIn: config.REFRESH_TOKEN_EXPIRY,
+    subject: 'refreshToken',
+  });
+};
+
+export const verifyAccessToken = (token) => {
+  return jwt.verify(token, config.JWT_ACCESS_TOKEN);
+};
+
+export const verifyRefreshToken = (token) => {
+  return jwt.verify(token, config.JWT_REFRESH_TOKEN);
+};
+
+export const createTokenCookie = ({ res, user }) => {
+  const payload = createPayload(user);
+
+  const refreshToken = generateRefreshToken({ payload });
+
+  res.cookie('refreshToken', refreshToken, {
+    httpOnly: true,
+    secure: config.NODE_ENV === 'production',
+    sameSite: 'strict',
+    maxAge: config.COOKIE_MAX_AGE,
+  });
+
+  return refreshToken;
+};
