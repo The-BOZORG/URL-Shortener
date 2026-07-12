4eacbed0de03b0cc8a86c670855ac1f382bd2869
soroush <soroushfalaki24@hotmail.com>
2026-07-12 18:27:14 +0330

    add login router

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import { register } from '../controllers/auth/register.js';
 import { verifyEmail } from '../controllers/auth/verifyEmail.js';
+import { login } from '../controllers/auth/login.js';
 
 const authRouter = Router();
 
 authRouter.post('/register', register);
 authRouter.get('/verify-email', verifyEmail);
+authRouter.post('/login', login);
 
 export default authRouter;
